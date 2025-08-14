d9dbd2f47527809069af6d28b6d2bc799f2ce30b
eliasDniel <eramosg2@unemi.edu.ec>
2025-08-14 10:32:33 -0500

    agregue animaciones al salir las peliculas

--- lib/presentation/widgets/movies/movies_poster_link.dart
+++ lib/presentation/widgets/movies/movies_poster_link.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
@@ -10,12 +12,21 @@ class MoviesPosterLink extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final random = Random();
     return FadeInUp(
+      from: random.nextInt(100) + 80,
+      delay: Duration(milliseconds: random.nextInt(450) + 0),
       child: GestureDetector(
         onTap: () => context.push('/home/2/movie/${movie.id}'),
         child: ClipRRect(
           borderRadius: BorderRadiusGeometry.circular(20),
-          child: Image.network(movie.posterPath!)),
+          child: FadeInImage(
+            height: 180,
+            fit: BoxFit.cover,
+            placeholder: AssetImage('assets/loaders/bottle-loader.gif'),
+            image: NetworkImage(movie.posterPath!),
+          ),
+        ),
       ),
     );
   }
